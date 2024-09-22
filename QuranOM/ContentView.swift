cfaab4d1ca3716932dec56ca26c962d6aacc3cdb
DevMoodie <Mahmoudyf96@gmail.com>
2024-09-22 00:33:50 -0400

    Initial Commit

---
+++ QuranOM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuranOM
+//
+//  Created by Moody on 2024-09-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuranOM/QuranOMApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuranOMApp.swift
+//  QuranOM
+//
+//  Created by Moody on 2024-09-22.
+//
+
+import SwiftUI
+
+@main
+struct QuranOMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
